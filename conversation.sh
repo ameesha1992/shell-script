8ef6bf1b11b5f83698732f9a2dac45ab5be97f5f
ameesha <amisha.bobbili@gmail.com>
2026-05-15 15:41:47 +0530

    first commit

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ramesh: hi how are you
+   ram: iam good how about you
+ramesh: me too what are you doing 
+  ram:  i was studying
+  ramesh:    i was learing devops  
+  ram: thats good
