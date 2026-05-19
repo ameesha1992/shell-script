cb6db38d08bbdd49fb7c7c1018f97e52311cc469
ameesha <amisha.bobbili@gmail.com>
2026-05-19 18:12:55 +0530

    message

--- redirectors.sh
+++ redirectors.sh
@@ -3,6 +3,8 @@ LOGS_FOLDER="/var/log/shell/"
 SCRIPT_NAME=$(echo $0|cut -d "." -f1) 
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
+sudo mkdir -p /var/log/shell/
+
 USAGE(){
    echo "USAGE:: sudo su redirectors.sh package1 package2.."
 
