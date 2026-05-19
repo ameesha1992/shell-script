822134ace9e350b60b4f29b33a7e19188515aecc
ameesha <amisha.bobbili@gmail.com>
2026-05-19 15:34:09 +0530

    message

--- redirectors.sh
+++ redirectors.sh
@@ -4,12 +4,14 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
 
-  for package in $@ &>>LOG_FILE
+  for package in $@ 
  do
    dnf list installed $package &>>$LOG_FILE
    if [ $? -ne 0 ]
    then
      echo "package not installed please install it" &>>$LOG_FILE
+     else
+     echo "package had already installed.."
    fi
  done
   
