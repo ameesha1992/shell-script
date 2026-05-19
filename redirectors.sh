30bcf39936c6ed9c06aa44ac4951f87dca5d6d23
ameesha <amisha.bobbili@gmail.com>
2026-05-19 18:07:21 +0530

    message

--- redirectors.sh
+++ redirectors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-LOGS_FOLDER="/var/log/shell"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1) 
-TIME_STAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOGS_FOLDER="/var/log/shell/"
+SCRIPT_NAME=$(echo $0|cut -d "." -f1) 
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
 USAGE(){
    echo"USAGE:: redirectors.sh package1 package2.."
