b41e43ab59d165104b09556e4f1969fa0c3480cc
ameesha <amisha.bobbili@gmail.com>
2026-05-19 16:00:13 +0530

    message

--- redirectors.sh
+++ redirectors.sh
@@ -3,6 +3,10 @@ LOGS_FOLDER="/var/log/shell"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) 
 TIME_STAMP=$(date +%y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
+USAGE(){
+   echo"USAGE:: redirectors.sh package1 package2.."
+
+}
 
   for package in $@ 
  do
